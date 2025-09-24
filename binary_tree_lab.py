dce7946b9ce0aed9643ea9b57b470affafa6a549
piarda <matt.rindini@gmail.com>
2025-09-24 17:04:47 -0400

    Implement max_depth and lowest_common_ancestor functions

--- binary_tree_lab.py
+++ binary_tree_lab.py
@@ -6,10 +6,21 @@ class TreeNode:
         self.left: Optional['TreeNode'] = None
         self.right: Optional['TreeNode'] = None
 
-# TODO: Implement the max_depth function
 def max_depth(root: Optional[TreeNode]) -> int:
-    pass
+    if root is None:
+        return 0
+    left_depth = max_depth(root.left)
+    right_depth = max_depth(root.right)
+    return max(left_depth, right_depth) + 1
 
-# TODO: Implement the lowest_common_ancestor function
 def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-    pass
+    # If both p and q are smaller than root, LCA is in left subtree
+    if p.val < root.val and q.val < root.val:
+        return lowest_common_ancestor(root.left, p, q)
+    
+    # If both p and q are greater than root, LCA is in right subtree
+    elif p.val > root.val and q.val > root.val:
+        return lowest_common_ancestor(root.right, p, q)
+    else:
+        # Found the split point otherwise (LCA node)
+        return root
